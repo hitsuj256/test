5d30cc287f491978a991a46668fcc0ecded6bfe6
hitsuj0005 <hitsuj0005@gmail.com>
2020-05-23 18:52:55 +0900

    uraddr.php追加

---
+++ uraddr.php
@@ -0,0 +1,17 @@
+<head>
+        <meta charset="UTF-8">
+        <title>確認くんもどき</title>
+</head>
+<body>
+	<?php
+	        echo "<h4>Your IP address and port number</h4>";
+	        echo $_SERVER["REMOTE_ADDR"],":",$_SERVER["REMOTE_PORT"],"<br><br>";
+		
+	        echo "<h4>HTTP Request Header</h4><table>";
+	        $headers = getallheaders();
+	        while (list ($header, $value) = each ($headers)) {
+	                echo "<tr><td>$header</td> <td>$value</td></tr>";
+	        };
+	        echo "</table>";
+	?>
+</body>
